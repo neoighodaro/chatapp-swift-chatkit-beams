ec8ee26533cb73bcd37aa8f9e00a438a6065bbe3
Neo Ighodaro <neo@creativitykills.co>
2018-07-09 13:35:51 +0100

    Move stuff around a little

---
+++ App/Convey/Classes/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  convey
+//
+//  Created by Neo Ighodaro on 09/07/2018.
+//  Copyright © 2018 TapSharp. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
