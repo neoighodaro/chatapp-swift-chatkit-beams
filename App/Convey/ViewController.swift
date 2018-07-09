62afc961e84903d000120f1e15c755c80bbd9ce8
Neo Ighodaro <neo@creativitykills.co>
2018-07-09 13:19:25 +0100

    Initial commit.

---
+++ App/Convey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  convey
+//
+//  Created by Neo Ighodaro on 09/07/2018.
+//  Copyright © 2018 TapSharp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
